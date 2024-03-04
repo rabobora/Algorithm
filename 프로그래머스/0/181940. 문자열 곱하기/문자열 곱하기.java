7c003d312cda3fd37085f6e9dbdb85a52435317d
rabobora <162130460+rabobora@users.noreply.github.com>
2024-03-05 08:59:04 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.12 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int k) {
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=1;i<=k;i++){
+            sb.append(my_string);
+        }
+        
+        return sb.toString();
+    }
+}
