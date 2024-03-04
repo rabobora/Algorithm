552705c95beb98c7578adddda3b2f2f8eb8c0045
rabobora <162130460+rabobora@users.noreply.github.com>
2024-03-05 08:57:07 +0900

    [level 0] Title: 문자 리스트를 문자열로 변환하기, Time: 0.03 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public String solution(String[] arr) {
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0;i<arr.length;i++){
+            sb.append(arr[i]);
+        }
+        
+        return sb.toString();
+    }
+}
