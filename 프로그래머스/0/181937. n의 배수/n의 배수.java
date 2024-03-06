7a22d12938aa3802c4dd65058279a650d7937b49
rabobora <162130460+rabobora@users.noreply.github.com>
2024-03-07 08:51:02 +0900

    [level 0] Title: n의 배수, Time: 0.02 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/181937. n의 배수/n의 배수.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int num, int n) {
+        if(num%n==0){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+}
